3dd25c941ea6c0754d6cd8660fcb4d2ec77f0dc7
young-nights <2863692760@qq.com>
2026-06-30 18:31:26 +0800

    fix: remap NNC6521 SPI pins to match dev board hardware
    - nnc6521_spi.c: update pin mapping table per hardware schematic
      Chip1: MOSI=PA7, CSN=PA4, SCLK=PA5, MISO=PA6, CHIP_EN=PC5, INTB=PC4
      Chip2: MOSI=PB15, CSN=PB12, SCLK=PB13, MISO=PB14, CHIP_EN=PC7, INTB=PC6
    - nnc6521_drv.c: update CHIP_EN power-up sequence to use correct GPIO pins
    - nnc6521.h: update pin mapping comments
    - main.c: add nnc6521_gpio_init() and dual-chip nnc6521_init() calls

---
+++ applications/main.c
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2006-2026, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2026-06-30     RT-Thread    first version
+ */
+
+#include <rtthread.h>
+
+#include "bsp_sys.h"
+#include "nnc6521.h"
+
+
+#define DBG_TAG "main"
+#define DBG_LVL DBG_LOG
+#include <rtdbg.h>
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* Initialize NNC6521 GPIO and both chips */
+  nnc6521_gpio_init();
+  nnc6521_init(NNC6521_CHIP_1);
+  nnc6521_init(NNC6521_CHIP_2);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
